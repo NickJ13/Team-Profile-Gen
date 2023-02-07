56beab233ca5197bf3390b95ba9ab67345d4aa02
Nick Stallard <stallardlax13@gmail.com>
2023-02-07 18:49:05 -0500

    readme file

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Team-Profile-Gen
+
+## Description
+This program will create an html file using the input from  a user. IT is an employee system that needs the name's, id's, and other questions, regarding what position they are.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+This requires inquierer
+
+## Usage
+ 
+How to: first open index.js in integrated terminal. after you've done so, you must install npm inquier v8.2.4. Then, in the integrated terminal type "node index" and will prompt you to answer a couple of questions. after all are answered it will generate a simple html file of the input you tryped in.
+
+## Credits
+
+N/A
+
+## License
+
+N/A
--- _test_/employee.test.js
+++ _test_/employee.test.js
@@ -1,5 +1,49 @@
 const Employee = require('../lib/employee');
 
 describe('Employee', () => {
+    it('should create an instance', () => {
+        let testClass = new Employee('Nick', 1, 'some@email.com')
+        expect(typeof testClass).toBe('object')
+    });
 
+    it('should set the name of the employee in the employee class', () => {
+        let testName = 'Nick';
+        let testClass = new Employee(testName, 1, 'Nick@email.com')
+        expect(testClass.name === testName).toBe(true)
+    });
+
+    it('should set the id of the employee in the employee class', () => {
+        let testId = 12;
+        let testClass = new Employee('zack', testId, 'zack@email.com')
+        expect(testClass.id === testId).toBe(true)
+    });
+
+    it('should set the email of the employee in the employee class', () => {
+        let testEmail = 'new@email.com';
+        let testClass = new Employee('zack', 3, testEmail)
+        expect(testClass.email === testEmail).toBe(true)
+    });
+
+    it('should get the name of the employeeb using the .getName method', () => {
+        let testName = 'tim';
+        let testClass = new Employee(testName, 3, 'tim@email.com')
+        expect(testClass.getName() === testName).toBe(true)
+    });
+
+    it('should get the id of the employee using the .getId method', () => {
+        let testId = 12;
+        let testClass = new Employee('John', testId, 'john@email.com')
+        expect(testClass.getId() === testId).toBe(true)
+    });
+
+    it('should get the email of the employee using the .getEmail method', () => {
+        let testEmail = 'new@email.com';
+        let testClass = new Employee('John', 10, testEmail)
+        expect(testClass.getEmail() === testEmail).toBe(true)
+    });
+
+    it('should get the role of the employee using the .getRole method', () => {
+        let testClass = new Employee('John', 10, 'john@email.com')
+        expect(testClass.getRole() === 'Employee').toBe(true)
+    });
 });
